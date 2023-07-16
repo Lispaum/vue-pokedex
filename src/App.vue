1e33c5078d35d56c337cc1cf5816948d2df8a7b5
lippe <felippemda@gmail.com>
2023-07-16 18:04:51 -0300

    refactor 2

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,16 @@ import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <!-- NAVBAR -->
+
+  <nav class="navbar navbar-expand-lg  bg-primary">
   <div class="container-fluid">
     
 
-    <router-link class="navbar-brand" to="/">Vue Pokedex</router-link>
+    <router-link class="navbar-brand" to="/">
+      <img src="https://cdn-icons-png.flaticon.com/512/188/188987.png" alt="pikachu icon" width="30" height="24" class="d-inline-block align-text-top">
+      Vue Pokedex
+    </router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -42,8 +47,46 @@ import HomeView from './views/HomeView.vue'
   </div>
 
 
+
+  <!-- FOOTER -->
+  <footer class="bg-primary text-light">
+    <div>Developed for a Technical Test</div>
+    |
+    <div>
+      <a href="asdasd">
+      Github Repository
+      </a>
+    </div>
+  </footer>
+
+
 </template>
 
 <style scoped>
+
+img{
+  object-fit: contain;
+}
+
+footer{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 3rem;
+}
+
+
+footer div {
+  margin: 0 1rem;
+}
+
+footer div a {
+  text-decoration: initial;
+  color: inherit;
+  font-weight: inherit;
+}
 
 </style>
